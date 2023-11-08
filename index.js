420935329c0cbdccbcfe46e0fceee5a5ddea5814
Fralou Maksim <fralou@protonmmail.com>
2023-11-08 19:22:13 +0300

    fix: main swiper

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  autoHeight: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  speed: 500,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    slideChange: function () {
+      document.querySelector('.mySwiper').scrollIntoView();
     },
-  });
+  },
+});
