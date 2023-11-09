b684d54a4f0beb1d58560474ad1764865b0f8f00
Fralou Maksim <fralou@protonmmail.com>
2023-11-09 15:56:15 +0300

    feat: add pagination

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   loop: true,
   speed: 500,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<div class="${className}">${index === 0 ? 'Женщины' : index === 1 ? 'Дети' : index + 1}</div>`;
+    },
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -18,3 +25,16 @@ var swiper = new Swiper(".mySwiper", {
 document.querySelector('.menu-list').addEventListener('click', event => {
     document.querySelector('.burger-checkbox').click();
 });
+
+var pagination = document.querySelector('.swiper-pagination');
+var footer = document.querySelector('.footer-title');
+
+var observer = new IntersectionObserver(function(entries) {
+  if (entries[0].isIntersecting) {
+    pagination.style.display = 'none';
+  } else {
+    pagination.style.display = 'flex';
+  }
+});
+
+observer.observe(footer);
